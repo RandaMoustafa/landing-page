8ed6fbf6c5d38a8441ed2f7a0e5fac4172747269
RandaMoustafa <fairystory93@gmail.com>
2021-03-18 16:26:25 +0200

    adding nav by js make every section appears active when scrolling down

--- js/app.js
+++ js/app.js
@@ -9,14 +9,18 @@
  * 
  * JS Version: ES2015/ES6
  * 
- * JS Standard: ESlint
+ * JS Standard: Eslint
  * 
 */
+
 
 /**
  * Define Global Variables
  * 
 */
+
+const nav = document.querySelectorAll('section');
+const navList = document.getElementById('navbar__list');
 
 
 /**
@@ -34,13 +38,46 @@
 */
 
 // build the nav
-
+nav.forEach(e => {
+    const navListElement = `<li class='menu__link ${e.className}' data-link=${e.id}>
+<a href="#${e.id}">${e.dataset.nav}</li>`
+    navList.insertAdjacentHTML('beforeend', navListElement);
+    //console.log(e);
+});
 
 // Add class 'active' to section when near top of viewport
+const callBack = top => {
+    top.forEach(nearTop => {
+        const navListElement = document.querySelector(
+            `.menu__link[data-link='${nearTop.target.id}']`,
+        )
+        const section = document.getElementById(nearTop.target.id);
 
+        if (nearTop && nearTop.isIntersecting) {
+            navListElement.classList.add('active')
+            section.classList.add('active')
+        } else {
+            if (navListElement.classList.contains('active')) {
+                navListElement.classList.remove('active')
+            }
+
+            if (section.classList.contains('active')) {
+                section.classList.remove('active')
+            }
+        }
+    });
+    //console.log(top);
+};
 
 // Scroll to anchor ID using scrollTO event
-
+navList.addEventListener('click', e => {
+    e.preventDefault();
+    const p = e.target.hasAttribute('data-link')
+        ? e.target
+        : e.target.parentElement;
+    const elementToScrollTo = document.getElementById(p.dataset.link);
+    elementToScrollTo.scrollIntoView({block: 'end', behavior: 'smooth'});
+});
 
 /**
  * End Main Functions
@@ -48,10 +85,19 @@
  * 
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
 
 
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.6,
+};
+const observer = new IntersectionObserver(callBack, options);
+nav.forEach(e => {
+    observer.observe(document.getElementById(e.id))
+});
